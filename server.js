0bfd5cf43cf0e36b307c343d0367a4aba585041a
RHos1 <109422736+RHos1@users.noreply.github.com>
2025-05-24 14:52:25 +0100

    Initialized Postgressql client in API code file

--- server.js
+++ server.js
@@ -1,4 +1,18 @@
 import express from 'express'
-app = express();
+import {Client}from "pg";
+import dotenv from 'dotenv';
+const app = express();
+dotenv.config();
 app.use(express.json());
+const client = new Client({
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    database: process.env.DB_NAME,
+});
+
+app.listen(8000, () => {
+    console.log('Running on port 8080');
+})
 

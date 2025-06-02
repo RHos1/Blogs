2642efe760568fc02623073c4bd9ab3eaa259f8d
RHos1 <109422736+RHos1@users.noreply.github.com>
2025-06-02 15:46:38 +0100

    Added postgresql to the techstack

---
+++ blog/blog-backend/Express/Database/postgres.js
@@ -0,0 +1,13 @@
+import {Client} from "pg";
+import dotenv from 'dotenv';
+dotenv.config();
+const client = new Client({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    port: Number(process.env.DB_PORT),
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+})
+
+await client.connect()
+console.log("Postgres Connected");
